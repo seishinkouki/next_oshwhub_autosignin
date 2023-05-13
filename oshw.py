5c7f907e80189ea474584109c949ffddb6e10f73
seishinkouki <sgflgg@gmail.com>
2023-05-13 22:29:15 +0800

    测试ql

--- oshw.py
+++ oshw.py
@@ -1,5 +1,6 @@
 import os
 import json
+from notify import send
 
 if "OSHW" in os.environ and len(os.environ["OSHW"]) > 1:
     try:
@@ -8,3 +9,6 @@ if "OSHW" in os.environ and len(os.environ["OSHW"]) > 1:
             print(key + ":" + users[key])
     except json.decoder.JSONDecodeError:
         print('用户名密码解析失败, 请检查环境变量中 OSHW 的格式')
+
+
+send("oshw签到通知", "测试1\r\n测试2\r\n测试3")
